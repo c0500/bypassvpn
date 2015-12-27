a9e0df7b51a761729ae7223bafdb3d6737406fa3
Brady Chen <chenchp@gmail.com>
2015-12-27 20:49:13 +0800

    format fix

--- bypassvpn.sh
+++ bypassvpn.sh
@@ -14,6 +14,8 @@ if [ $# -lt 1 ];then
     usage
 fi
 
+ipdataurl="http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
+
 function dnsToIPs() {
     ips=`dig +noall +answer $1 | awk '$4=="A" {print $5}'`
     echo $ips
@@ -22,7 +24,9 @@ function dnsToIPs() {
 gw=`netstat -rn | grep  default | grep -v utun | awk '{print $2}' 2>/dev/null`
 
 if [ "$1" = "ALL" ];then
-    ips=`curl -s http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest | awk -F '|' '$2 == "CN" && $3 == "ipv4" && $7 == "allocated" { printf "%s/%d\n",$4,32-log($5)/log(2) }'`
+    ips=`curl -s $ipdataurl \
+        | awk -F '|' '$2 == "CN" && $3 == "ipv4" && $7 == "allocated" \
+            { printf "%s/%d\n",$4,32-log($5)/log(2) }'`
     for ip in $ips; do
         echo -n "set routing: $ip to $gw ..."
         if route -nv add $ip $gw >/dev/null 2>&1; then
